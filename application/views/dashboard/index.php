95281c70cd22ef683bb5b2c8f7e03ec92469f362
joseluis gomez <joseluisgomezcecegna@gmail.com>
2024-06-17 00:16:02 -0700

    backup.

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,73 @@
+<div class="row">
+                        
+<div class="col-md-6 col-lg-3">
+                            <a href="">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div>
+                                                <p class="m-b-0">Propiedades</p>
+                                                <h2 class="m-b-0">
+                                                    <span></span>
+                                                </h2>
+                                            </div>
+                                            <div class="avatar avatar-icon avatar-lg avatar-blue">
+                                                <i class="anticon anticon-home"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div>
+                                            <p class="m-b-0">Bounce Rate</p>
+                                            <h2 class="m-b-0">
+                                                <span>26.80%</span>
+                                            </h2>
+                                        </div>
+                                        <div class="avatar avatar-icon avatar-lg avatar-cyan">
+                                            <i class="anticon anticon-bar-chart"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div>
+                                            <p class="m-b-0">Orders</p>
+                                            <h2 class="m-b-0">
+                                                <span>3057</span>
+                                            </h2>
+                                        </div>
+                                        <div class="avatar avatar-icon avatar-lg avatar-red">
+                                            <i class="anticon anticon-profile"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div>
+                                            <p class="m-b-0">Total Expense</p>
+                                            <h2 class="m-b-0">
+                                                <span>$6,138</span>
+                                            </h2>
+                                        </div>
+                                        <div class="avatar avatar-icon avatar-lg avatar-gold">
+                                            <i class="anticon anticon-bar-chart"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
